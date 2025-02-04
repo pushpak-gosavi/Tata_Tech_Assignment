e70c66a699957a47c0205c19d7c669b08d872a1c
Pushpak Gosavi <pushpak.gosavi7@gmail.com>
2025-02-04 18:30:29 +0530

    Initial commit

---
+++ app/src/main/java/com/pushpak/tatatechassignment/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.pushpak.tatatechassignment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.pushpak.tatatechassignment.ui.theme.TataTechAssignmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TataTechAssignmentTheme {
+                GenerateData()
+            }
+        }
+    }
+}
+
+@Composable
+fun GenerateData() {
+    var number by remember { mutableStateOf("") }
+    var randomString by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Spacer(Modifier.height(100.dp))
+        Text(
+            modifier = Modifier.padding(horizontal = 10.dp),
+            textAlign = TextAlign.Left,
+            text = "Please Enter number in belows edit text for getting the random string from the existing application with specified length",
+        )
+        Spacer(Modifier.height(10.dp))
+        OutlinedTextField(
+            value = number,
+            onValueChange = { number = it },
+            placeholder = {
+                Text("Please Enter a Number")
+            },
+            label = {
+                Text("Please Enter a Number")
+            },
+            textStyle = TextStyle(color = Color.Black),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Spacer(Modifier.height(10.dp))
+        Button(onClick = {
+            if (number != "")
+                randomString = getRandomString(number.toInt())
+        }) {
+            Text("Get Random String")
+        }
+        Spacer(Modifier.height(10.dp))
+        Text(text = randomString)
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun PreviewGenerateData() {
+    GenerateData()
+}
+
+fun getRandomString(length: Int): String {
+    val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+    return (1..length)
+        .map { allowedChars.random() }
+        .joinToString("")
+}
+
